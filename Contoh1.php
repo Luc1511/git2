595c3784a81376ddceee6199fc5e50be473ce5da
Luc1511 <luqmannuryusufsyah@gmail.com>
2023-09-26 19:23:59 +0700

    menambahkan BaseController.php, Contoh1.php, Home.php,latihan1.php

---
+++ Contoh1.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+class Contoh1 extends BaseController
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Luqman
+            Saya tinggal di sukatani
+            Olahraga kesukaan saya renang";
+    }
+}
---
+++ latihan1.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controllers;
+
+class latihan1 extends BaseController
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama : Luqman Nur Yusuf<p>
+        Kelas : 12.3B.24<p>
+        NIM : 12220584<p>
+        Tempat Tanggal lahir : Bekasi, 15 November 2001<p>
+        Hobi : Game";
+    }
+}
